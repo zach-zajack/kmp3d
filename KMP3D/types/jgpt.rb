4dc199c875132ccfeffb4e28eebf88273bb0259c
Zach Zajack <zachzajack@gmail.com>
2019-07-24 01:11:42 +0000

    update jpt settings for index

--- KMP3D/types/jgpt.rb
+++ KMP3D/types/jgpt.rb
@@ -2,7 +2,10 @@ module KMP3D
   class JGPT < Type
     def initialize
       @name = "Respawns"
-      @settings = [Settings.new(:int, "Range", "0")]
+      @settings = [
+        Settings.new(:int, "Index", "0"),
+        Settings.new(:int, "Range", "0")
+      ]
       super("vector")
     end
   end

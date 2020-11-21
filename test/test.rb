318ccf32d0e4b95555ac11df231649b745a9b325
zach-zajack <zachzajack@gmail.com>
2020-11-21 15:34:49 -0500

    :construction: Replace random test with menu of every file in directory

--- test/test.rb
+++ test/test.rb
@@ -2,18 +2,22 @@ module KMP3D
   require "#{DIR}/test/kmp3d_test"
   require "#{DIR}/test/test_import_export"
 
-  rebuild_random = UI::Command.new("1:1 Rebuild, Random file") do
-    path = Dir["#{DIR}/test/kmps/*.kmp"].shuffle.first
-    TestImportExport.new(path)
+  menu = UI.menu.add_submenu("KMP3D Tests")
+
+  rebuild_files = menu.add_submenu("1:1 Rebuild file")
+
+  Dir["#{DIR}/test/kmps/*.kmp"].each do |path|
+    cmd = UI::Command.new(path[path.rindex(%r{[\\/]}) + 1..-1]) do
+      TestImportExport.new(path)
+    end
+    rebuild_files.add_item(cmd)
   end
 
-  rebuild_all = UI::Command.new("1:1 Rebuild, All files") do
+  rebuild_all = UI::Command.new("1:1 Rebuild all files") do
     Dir["#{DIR}/test/kmps/*.kmp"].each do |path|
       TestImportExport.new(path)
     end
   end
 
-  menu = UI.menu.add_submenu("KMP3D Test")
-  menu.add_item(rebuild_random)
   menu.add_item(rebuild_all)
 end

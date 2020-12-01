1d54ed42a85859b7a570f4034c99cde9f6d6d079
Zach Zajack <zachzajack@gmail.com>
2020-11-30 21:03:16 -0500

    :bookmark: Bump version

--- kmp3d_loader.rb
+++ kmp3d_loader.rb
@@ -6,7 +6,7 @@ module KMP3D
 
   unless file_loaded?(__FILE__)
     kmp3d = SketchupExtension.new("KMP3D", "#{DIR}/lib/kmp3d")
-    kmp3d.version = "6.0"
+    kmp3d.version = "7.0"
     kmp3d.description = "A 3D interface for KMP files."
     Sketchup.register_extension(kmp3d, true)
     file_loaded(__FILE__)

b1d3ec89570c0bcd9b10adba7bb467675d2ab500
zach-zajack <zachzajack@gmail.com>
2020-12-11 22:22:55 -0500

    :bug: Fix STGI flare color alignment

--- lib/kmp3d/types/stage_info.rb
+++ lib/kmp3d/types/stage_info.rb
@@ -11,8 +11,8 @@ module KMP3D
         Settings.new(
           :dropdown, :byte, "Lens Flare", "0", %w[Enabled Disabled]
         ),
-        Settings.new(:hidden, :byte, "Padding", "0"),
-        Settings.new(:text, :uint32, "Flare Color (RGBA)", "0xE6E6E64B"),
+        Settings.new(:text, :uint32, "Flare Color (ARGB)", "0xE6E6E6"),
+        Settings.new(:hidden, :byte, "Unknown", "75"),
         Settings.new(:text, :float, "Speed Modifier", "1.0")
       ]
       super
